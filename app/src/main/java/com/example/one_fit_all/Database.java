8b1ee73baae3310105e529402d81ff80467549fe
mowitski <imskowitz33@gmail.com>
2021-05-07 21:03:44 -0700

    Changed UserInfo.xml and made minor updates to code to potentially fix the problem on the User Info page

--- app/src/main/java/com/example/one_fit_all/Database.java
+++ app/src/main/java/com/example/one_fit_all/Database.java
@@ -76,7 +76,7 @@ public class Database extends SQLiteOpenHelper {
         contentValues.put(COLUMN_USER_FEET, feet);
         contentValues.put(COLUMN_USER_INCH, inch);
         // contentValues.put(COLUMN_USER_GENDER, customerClass.getGender())
-       Cursor cursor = db.rawQuery("Select * From USER_TABLE where name = ?", new String[] {name});
+       Cursor cursor = db.rawQuery("Select * From USER_TABLE where COLUMN_USER_NAME = ?", new String[] {String.valueOf(name)});
        if(cursor.getCount() > 0) {
            long update = db.update(USER_TABLE, contentValues, "name=?", new String[]{name});
            if (update == -1) {
@@ -95,7 +95,7 @@ public class Database extends SQLiteOpenHelper {
 
     public Cursor getData() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("Select * From USER_TABLE where name = ?", null);
+        Cursor cursor = db.rawQuery("Select * From USER_TABLE where COLUMN_USER_NAME = ?", null);
         return cursor;
     }
    /* public List<CustomerClass> getEveryone() {
